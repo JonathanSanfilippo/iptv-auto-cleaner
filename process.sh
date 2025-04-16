8a0c7b88c4d519c58af7dfdcba75da275cc375f5
Jonathan <jonalinux.uk@gmail.com>
2025-04-16 16:07:33 +0100

    Update process.sh

--- process.sh
+++ process.sh
@@ -4,37 +4,40 @@ REPO_DIR="$(dirname "$(readlink -f "$0")")"
 SRC_DIR="$REPO_DIR/lists/original"
 DEST_DIR="$REPO_DIR/lists/processed"
 URLS_FILE="$REPO_DIR/urls.txt"
+SRC_FILE="$SRC_DIR/original.m3u"
 OUTFILE="$DEST_DIR/altre.m3u"
+UPDATE_FILE="$REPO_DIR/lists/last_update.txt"
 
+# Crea directory se non esistono
 mkdir -p "$SRC_DIR" "$DEST_DIR"
-echo "#EXTM3U" > "$OUTFILE"
 
-# Scarica le liste nella cartella original/
-index=1
+# Pulisce eventuali file precedenti
+rm -f "$SRC_FILE" "$OUTFILE"
+
+# Scarica tutte le liste IPTV e uniscile in un solo file
+> "$SRC_FILE"  # svuota/crea il file
+
 while read -r url; do
   [[ -z "$url" ]] && continue
-  fname="lista${index}.m3u"
-  curl -s "$url" -o "$SRC_DIR/$fname"
-  ((index++))
+  curl -s "$url" >> "$SRC_FILE"
 done < "$URLS_FILE"
 
-# Estrai i canali e scrivi nella lista "Altre Liste"
-for file in "$SRC_DIR"/*.m3u; do
-  awk -v output="$OUTFILE" '
-    BEGIN { RS="\r?\n"; FS="," }
-    /^#EXTINF/ {
-      name = $2
-      if (name ~ /\[COLOR|\[B|\]/ || name == "") next
-      getline url
-      if (url ~ /^http/) {
-        gsub(/\s+$/, "", name)
-        printf "#EXTINF:-1 tvg-name=\"%s\" tvg-logo=\"https://img.icons8.com/office40/512/raspberry-pi.png\" tvg-id=\"\" group-title=\"Altre Liste\",%s\n%s\n\n", name, name, url >> output
-      }
+# Crea il file M3U di output
+echo "#EXTM3U" > "$OUTFILE"
+
+# Estrai i canali validi da original.m3u e scrivi in altre.m3u
+awk -v output="$OUTFILE" '
+  BEGIN { RS="\r?\n"; FS="," }
+  /^#EXTINF/ {
+    name = $2
+    if (name ~ /\[COLOR|\[B|\]/ || name == "") next
+    getline url
+    if (url ~ /^http/) {
+      gsub(/\s+$/, "", name)
+      printf "#EXTINF:-1 tvg-name=\"%s\" tvg-logo=\"https://img.icons8.com/office40/512/raspberry-pi.png\" tvg-id=\"\" group-title=\"Altre Liste\",%s\n%s\n\n", name, name, url >> output
     }
-  ' "$file"
-done
+  }
+' "$SRC_FILE"
 
-
-# Scrive un file con l'ultimo aggiornamento
-UPDATE_FILE="$REPO_DIR/lists/last_update.txt"
+# Scrive data/ora aggiornamento
 echo "Last Update list: $(date '+%d-%m-%y %H:%M')" > "$UPDATE_FILE"
